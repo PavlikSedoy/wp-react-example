3016cafb9df4f40d415a25d346622f23d63032ac
PavelSedoy <pavliksedoy@gmail.com>
2019-07-16 22:21:51 +0300

    Frontend. Get books from API

---
+++ frontend/src/components/BookItem.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
+export class BookItem extends Component {
+  state = {
+    imgUrl: '',
+    author: '',
+    isLoaded: false
+  }
+
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    const { featured_media, author } = this.props.book
+    const getImageUrl = axios.get(`/wp-json/wp/v2/media/${featured_media}`)
+    const getAuthor = axios.get(`/wp-json/wp/v2/users/${author}`)
+
+    Promise.all([getImageUrl, getAuthor])
+      .then(res => {
+        this.setState({
+          imgUrl: res[0].data.media_details.sizes.full.source_url,
+          author: res[0].data.name,
+          isLoaded: true
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const { title, excerpt } = this.props.book
+    const { author, imgUrl, isLoaded } = this.state
+
+    if (isLoaded) {
+      return (
+        <div>
+          <h2 style={{ marginBottom: '0' }}>{ title.rendered }</h2>
+          <small>Review by <strong>{ author }</strong></small>
+          <img 
+            style={{ width: '100%' }} 
+            src={ imgUrl }
+            alt={title.rendered}
+          />
+          <div dangerouslySetInnerHTML={{ __html: excerpt.rendered }}></div>
+        </div>
+      )
+    }
+
+    return null
+  }
+}
+
+export default BookItem
